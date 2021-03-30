5ac7d6fefd69689e1a2b7b0eb6cf68f392f236ce
KimJinSeong-Git <acmiheee@gmail.com>
2021-03-30 19:45:15 +0900

    Add Lec2 Summery

---
+++ ML-DL for Everone Season2_Tensorflow/Lec2_Simple Linear Regression/Lec2_SimpleLinearRegressionLAB.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+
+# Data
+x_data = [1, 2, 3, 4, 5]
+y_data = [1, 2, 3, 4, 5]
+
+# W, b initialize
+W = tf.Variable(2.9)
+b = tf.Variable(0.5)
+
+learning_rate = 0.01
+
+for i in range(100+1): # W, b update
+    # Gradient Descent
+    with tf.GradientTape() as tape:
+        hypothesis = W * x_data + b
+        cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+
+    W_grad, b_grad = tape.gradient(cost, [W, b])
+
+    W.assign_sub(learning_rate * W_grad)
+    b.assign_sub(learning_rate * b_grad)
+
+    if i % 10 == 0:
+        print("{:5}|{:10.4f}|{:10.4}|{:10.6f}".format(i, W.numpy(), b.numpy(), cost))
+
+"""
+[ 실행결과 ]
+    0|    2.4520|     0.376| 45.660004
+   10|    1.1036|  0.003398|  0.206336
+   20|    1.0128|  -0.02091|  0.001026
+   30|    1.0065|  -0.02184|  0.000093
+   40|    1.0059|  -0.02123|  0.000083
+   50|    1.0057|  -0.02053|  0.000077
+   60|    1.0055|  -0.01984|  0.000072
+   70|    1.0053|  -0.01918|  0.000067
+   80|    1.0051|  -0.01854|  0.000063
+   90|    1.0050|  -0.01793|  0.000059
+  100|    1.0048|  -0.01733|  0.000055
+"""
--- ML-DL for Everone Season2_Tensorflow/Lec2_Simple Linear Regression/README.md
+++ ML-DL for Everone Season2_Tensorflow/Lec2_Simple Linear Regression/README.md
@@ -18,3 +18,11 @@
 
 ### 5. Goal : Minimize Cost
 > 위의 내용에 대한 결과로 Learning이란 Cost를 정의하고 이를 최소화하는 과정을 말한다.
+
+
+### 6. Gradient Descent Algorithm
+> 보편적인 Cost 최소화 알고리즘
+> 초기값 부터 Weight, Bias 값의 기울기를 조정해 나가며 해당하는 방정식을 찾아간다.
+> Gradient Tape : 변수들을 기록하는 역할이며, 후에 변수들에 대한 기울기 값. 즉, 미분값을 반환하기 위함
+> Learning rate : Gradient 값을 얼마만큼 적용할지 정해주는 변수( 매우 작은 값을 사용 )
+> Epoch : 반복회수
